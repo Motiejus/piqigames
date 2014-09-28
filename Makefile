b1742d05b5c8d40c440694beb3b36f2ac60d10c8
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-09-28 21:34:15 +0200

    Add more stuff to template

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: defn
 
+definition/demo.html: piqigames
+	./piqigames -in=definition/demo.piqi.pb -out=definition/demo.html
+
 piqigames: docgen.go defn
 	go build
 
--- templates/tpl.go
+++ templates/tpl.go
@@ -1,52 +1,53 @@
 package templates
-
 import "html/template"
+var Details = template.Must(template.New("details").Parse(`
 
-var Details = template.Must(template.New("details").Parse(`<html>
-{{ range . }}
+<html>
+{{ range . }}{{ with $piqi := . }}{{/* top-level 'Piqi' elements */}}
 
-<h1>Module '{{ .Module }}'</h1>
+<h1>Module : {{ $piqi.Module }}</h1>
 
 <h2>Type Definitions</h2>
+{{ range .PiqiTypedef }} {{ with $t := . }}
 
-{{ range .PiqiTypedef }}
+  {{ if .Record }}
+  <h3 name="{{ $piqi.Module }}_{{ .Record.Name }}">Record : {{ .Record.Name }}</h3>
+    <ul>
+      {{ range .Record.Field }}
+      <li>Field : {{ .Name }} ({{ .Type}})</li>
+      {{ end }} {{/* Field */}}
+    </ul>
+  {{ else if .Alias }}
+    <h3>Alias : {{ .Alias.Name }}</h3>
+    Type: {{ .Alias.Type }}
+  {{ else if .Variant }}
+    <h3>Variant : {{ .Variant.Name }}</h3>
+    Not implemented
+  {{ else if .List }}
+    <h3>List : {{ .List.Name }}</h3>
+  {{ end }} {{/* else if .List */}}
 
-{{ if .Record }}
-<h3>Record '{{ .Record.Name }}'</h3>
-<ul>
-  {{ range .Record.Field }}
-  <li>Field '{{ .Name }}' ({{ .Type}})</li>
-  {{ end }} {{/* Field */}}
-</ul>
-{{ else if .Alias }}
-<h3>Alias '{{ .Alias.Name }}'</h3>
-Type: {{ .Alias.Type }}
-{{ else if .Variant }}
-<h3>Variant '{{ .Variant.Name }}'</h3>
-Not implemented
-{{ else if .List }}
-<h3>List '{{ .List.Name }}'</h3>
+{{ end }} {{/* range .PiqiTypedef */}}
 {{ end }}
-
-
-{{ end }} {{/* PiqiTypedef */}}
 
 <h2>Methods</h2>
 {{ range .Function }}
-<h3>{{ .Name }}</h3>
-<h4>Input</h4>
-{{ .Input }}
+  <h3>Function : {{ .Name }}</h3>
+  <h4>Input</h4>
+  {{ .Input }}
 
-<h4>Output</h4>
-{{ .Output }}
+  <h4>Output</h4>
+  {{ .Output }}
 
-<h4>Error</h4>
-{{ if .Error }}
-{{ .Error }}
-{{ else }}
-N/A
-{{ end }} {{/* if error */}}
+  <h4>Error</h4>
+  {{ if .Error }}
+  {{ .Error }}
+  {{ else }}
+  N/A
+  {{ end }} {{/* if .Error */}}
 
-{{ end }} {{/* function */}}
-{{ end }} {{/* toplevel PiqiList */}}
-</html>`))
+  {{ end }} {{/* range .Function */}}
+{{ end }}{{ end }} {{/* toplevel PiqiList */}}
+</html>
+
+`))
